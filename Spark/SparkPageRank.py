fc948e203379afa0827b5be4454e0e4938087ee9
Leonardo Poggiani <leonardo.poggiani@gmail.com>
2021-06-01 17:43:14 +0200

    function filterLinks

--- Spark/SparkPageRank.py
+++ Spark/SparkPageRank.py
@@ -1,14 +1,22 @@
-import re
-
 from pyspark import SparkContext
 import re
 
+
+def filterLinks(title, links, page):
+    outlinks = re.findall("\\[\\[(.*?)\\]\\]", links)
+    list(filter(lambda k: '|' in k, outlinks))
+
+    if title in outlinks
+
+
+
 def parsePages(page):
     title = re.findall("<title>(.*)</title>", page)
-    text = re.findall("<text>(.*?)</text>")
-    outlinks = page.findall("\[\[(.*?)\]\]")
+    text = re.findall("<text>(.*?)</text>", page)
+    outlinks = filterLinks(title, text)
 
     return title[0], outlinks
+
 
 sc = SparkContext(appName="WikiPageRank", master="yarn")
 sc.setLogLevel("ERROR")
