e539c27b8a1076bed6da5c0de310c6a6075eca67
Leonardo Poggiani <leonardo.poggiani@gmail.com>
2021-06-04 22:11:03 +0200

    updated new version

--- Spark/SparkPageRank.py
+++ Spark/SparkPageRank.py
@@ -1,72 +1,78 @@
 from pyspark import SparkContext
 import re
 import pprint
-from operator import add
 
 
-# filter valid links using regex
 def filterLinks(title, links):
     wiki_piped_links = re.findall("\\[\\[(.*?)\\]\\]", links)  # found all links
     wiki_links = []
 
-    for every_link in wiki_piped_links:
-        splitted = every_link
+    for link in wiki_piped_links:
+        splitted = link
 
-        if "|" in every_link:  # if we have a pipe we must parse the link:
-            lastIndexOfPipe = splitted.rindex("|", 0, len(splitted))  # 1) splitting links with pipes
-            splitted = splitted[0:lastIndexOfPipe]  # 2) keeping only the part before the pipe
+        if "|" in link:
+            lastIndexOfPipe = splitted.rindex("|", 0, len(splitted))  # splitting links with pipes
+            splitted = splitted[0:lastIndexOfPipe]  # keeping only the part before the pipe
 
         if (splitted == title) or (splitted in wiki_links):  # link already present or auto-referencing
             continue
 
-        wiki_links.append(splitted)  # list of parsed links
+        wiki_links.append(splitted.strip())  # list of parsed links
 
     return wiki_links
 
 
-# we receive an xml file, we must parse it
-def parsePages(page, lines):
+def parsePages(page):
     title = re.findall("<title>(.*)</title>", page)
     text = re.findall("<text(.*?)</text>", page)
-    outlinks = filterLinks(title[0], text[0])
+    outlinks = filterLinks(title[0].strip(), text[0])
 
-    return title[0], outlinks, float(1 / lines)  # return: title | <outlinks> | 1/N
-
-
-# compute the rank to send
-def computeContributions(pages, pageRank):
-    count = len(pages)
-    for page in pages:
-        yield page, pageRank / count
+    return title[0].strip(), outlinks
 
 
-# ****** PAGE RANK SPARK ****** #
-sc = SparkContext(appName="WikiPageRank", master="yarn")
-sc.setLogLevel("ERROR")
+def distributeRankToOutlinks(father, outlinks, rank):
+    num_outlinks = len(outlinks)
+    list = [(father, 0)]
+    for link in outlinks:
+        list.append((link, rank / num_outlinks))
+    return list
 
-wiki_micro = sc.textFile("hdfs://namenode:9820/user/hadoop/input/wiki-micro.txt")
 
-number_of_lines = wiki_micro.count()
-mapPages = wiki_micro.map(lambda e: parsePages(e, number_of_lines))  # reading and parsing input file
+def computeNewRank(lastRank):
+    if lastRank == 0:
+        return float(1.0 / numberOfPages)
+    else:
+        return float((0.15 * (float(1 / numberOfPages)) + 0.85 * lastRank))
 
-pageRanks = []
 
-for iteration in range(10):
-    contributions = mapPages.flatMap(lambda x: computeContributions(x[1], x[2]))
-    pageRanks = contributions.reduceByKey(add).mapValues(lambda r: 0.15 * float(1 / number_of_lines) + 0.85 * r)
+if __name__ == "__main__":
 
-pageRanksOrdered = pageRanks.takeOrdered(10, key=lambda x: -x[1])  # take the first 10 links ordered
-fileToSave = sc.parallelize(pageRanksOrdered)
-fileToSave.saveAsTextFile('sparkOutput_2.txt')
+    sc = SparkContext(appName="WikiPageRank", master="yarn")
+    sc.setLogLevel("ERROR")
 
-i = 0
-rankPrecedente = 0
+    lines = sc.textFile("hdfs://namenode:9820/user/hadoop/input/wiki-micro.txt")
 
-for (link, rank) in pageRanksOrdered:
-    if rankPrecedente != rank:
-        i += 1
+    titles = lines.map(lambda page: parsePages(
+        page)).cache()  # esempio di tupla --> ('Image:Lynne Slater2.jpg', ['EastEnders', 'Lynne Hobbs', 'Category:EastEnders images'])
 
-    print("%i: Title: %s Rank: %s" % (i, link, rank))
-    rankPrecedente = rank
+    numberOfPages = titles.count()
 
-sc.stop()
+    ranks = titles.map(lambda page: (page[0], float(1.0 / numberOfPages)))
+
+    for iteration in range(10):
+        contributions = titles.join(ranks).flatMap(
+            lambda page: distributeRankToOutlinks(page[0], page[1][0], page[1][1]))
+
+        ranks = contributions.reduceByKey(lambda x, y: x + y).mapValues(lambda rank: computeNewRank(rank))
+
+    # titles = []
+    # for result in ranks.collect():
+    #     if(result[0] in titles):
+    #         print("Doppione",result[0])
+    #     titles.append(result[0])
+
+    pageRanksOrdered = ranks.takeOrdered(ranks.count(), key=lambda x: -x[1])
+    pageRanksOrdered = sc.parallelize(pageRanksOrdered)
+    pageRanksOrdered.saveAsTextFile('sparkOutput_giulio_prova1.txt')
+
+    sc.stop()
