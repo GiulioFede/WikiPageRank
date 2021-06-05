71bb56a9cd5aff0526acd623862c44c33d63ae91
giulio federico <giuliof3derico@gmail.com>
2021-06-05 17:01:47 +0200

    cleaning

--- Hadoop/src/main/java/it/unipi/hadoop/dataModel/CustomCounter.java
+++ Hadoop/src/main/java/it/unipi/hadoop/dataModel/CustomCounter.java
@@ -2,9 +2,6 @@ package it.unipi.hadoop.dataModel;
 
 public enum CustomCounter {
     NUMBER_OF_PAGES,
-    NUMBER_OF_RANK_ITERATIONS,
     ULTIMO_MAP,
-    ULTIMO_REDUCE,
-    SUM,
     NUMBER_OF_CHILD
 }
