b595ffd02771ef440d9a013d923836ae86d46d4d
LEO\Leo <leomaltese95@gmail.com>
2021-06-07 01:59:06 +0200

    fix

--- Hadoop/src/main/java/it/unipi/hadoop/WikiPageRank.java
+++ Hadoop/src/main/java/it/unipi/hadoop/WikiPageRank.java
@@ -120,14 +120,14 @@ public class WikiPageRank
                 System.out.println("Job failed: page rank could not be calculated");
                 System.exit(0);
             }
-/*
+
             if(Integer.parseInt(computePageRank_job.getConfiguration().get("convergence")) == 0){
                     System.out.println("Convergence is achieved after "+ (i+1)+" iterations" +
                             " Value:" +nPagesAndOutlinks_job.getCounters().findCounter(CustomCounter.CONVERGENCE).getValue());
                     i++;
                     break;
                 }
-*/
+
 
         }
 
