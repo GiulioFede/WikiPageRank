36d6213f7ef8b6a3a78bee4192ec4cdb968daaa1
Leonardo Poggiani <leonardo.poggiani@gmail.com>
2021-06-08 15:34:49 +0200

    passing iterations as a parameter

--- Spark/Python/SparkPageRank.py
+++ Spark/Python/SparkPageRank.py
@@ -3,7 +3,8 @@ import re
 import sys
 import time
 
-#find the all links in <text>...</text>, filter and format some particular links
+
+# find the all links in <text>...</text>, filter and format some particular links
 def filterLinks(title, links):
     wiki_piped_links = re.findall("\\[\\[(.*?)\\]\\]", links)  # found all links
     wiki_links = []
@@ -24,20 +25,22 @@ def filterLinks(title, links):
 
         wiki_links.append(splitted.strip())  # list of parsed links
 
-    return wiki_links #return the 'title' outlinks
+    return wiki_links # return the 'title' outlinks
 
-#compute and retrieve the title and its adjacencyList (outlinks)
+
+# compute and retrieve the title and its adjacencyList (outlinks)
 def parsePages(page):
-    title = re.findall("<title>(.*)</title>", page) #retrieve title of pages
-    text = re.findall("<text(.*?)</text>", page) #retrieve all the text between <text>...</text>
+    title = re.findall("<title>(.*)</title>", page) # retrieve title of pages
+    text = re.findall("<text(.*?)</text>", page) # retrieve all the text between <text>...</text>
     outlinks = filterLinks(title[0].strip(), text[0])
 
     return title[0].strip(), outlinks
 
-#function in flatMap transformation
+
+# function in flatMap transformation
 def distributeRankToOutlinks(father, outlinks, rank):
     num_outlinks = len(outlinks)
-    list = [(father, 0)] # to include the 'father' of the outlinks in the list
+    list = [(father, 0)]  # to include the 'father' of the outlinks in the list
     for link in outlinks:
         list.append((link, rank / num_outlinks))
     return list
@@ -52,42 +55,43 @@ if __name__ == "__main__":
         print("Number of parameters are " + str(len(sys.argv))+"! The number of parameters must be equal to three!")
         sys.exit(1)
 
-    #initialize a new Spark Context
+    # initialize a new Spark Context
     sc = SparkContext(appName="WikiPageRank", master="yarn")
 
-    #delete some log lines (this option can be removed)
+    # delete some log lines (this option can be removed)
     sc.setLogLevel("ERROR")
 
     # build an RDD from input file
-    lines = sc.textFile(address + str(sys.argv[1]))
+    lines = sc.textFile(address + str(sys.argv[2]))
+
+    # retrive number of iterations
+    iterations = sys.argv[1]
 
     # map transformation on each line
     # example of tuple-->('title', ['outlink1', 'outlink2', ....., 'outlinkN'])
     titles = lines.map(lambda page: parsePages(page)).cache()
 
-    #Count number of pages
+    # Count number of pages
     numberOfPages = titles.count()
 
-    #initialize rank for each page with 1/numberOfPages
-    #example of (K,V) structure --> ('title', InitialPageRank)
+    # initialize rank for each page with 1/numberOfPages
+    # example of (K,V) structure --> ('title', InitialPageRank)
     ranks = titles.map(lambda page: (page[0], float(1.0 / float(numberOfPages))))
 
-    #cicle for compute the PageRank of pages
-    for iteration in range(10):
-        #JOIN TRANSFORMATION -->combine outlinks and pageRank of a page
-        #example of structure of contributions AFTER join: [('title', ([listOfOutlinks], pageRank))....]
-        #FLATMAP TRANSFORMATION --> compute the rank to be distributed among the outlinks
-        #example of structure of contributions AFTER flatMap: [('title', incomingPageRank)....]
+    # cicle for compute the PageRank of pages
+    for iteration in range(iterations):
+        # JOIN TRANSFORMATION -->combine outlinks and pageRank of a page
+        # example of structure of contributions AFTER join: [('title', ([listOfOutlinks], pageRank))....]
+        # FLATMAP TRANSFORMATION --> compute the rank to be distributed among the outlinks
+        # example of structure of contributions AFTER flatMap: [('title', incomingPageRank)....]
         contributions = titles.join(ranks).flatMap(lambda page: distributeRankToOutlinks(page[0], page[1][0], page[1][1]))
 
-        #REDUCEBYKEY TRANSFORMATION --> sum all incomingPageRanks of a page --> (page, partialPageRank)
-        #MAPVALUES TRANSFORMATION --> compute the PageRank of the page with the formula
+        # REDUCEBYKEY TRANSFORMATION --> sum all incomingPageRanks of a page --> (page, partialPageRank)
+        # MAPVALUES TRANSFORMATION --> compute the PageRank of the page with the formula
         ranks = contributions.reduceByKey(lambda x, y: x + y).mapValues(lambda rank: computeNewRank(rank))
 
-    #Order the pages by PageRank and save the total rank in a file
-    #pageRanksOrdered = ranks.takeOrdered(ranks.count(), key=lambda x: -x[1])
-    #pageRanksOrdered = sc.parallelize(pageRanksOrdered)
-    #pageRanksOrdered.saveAsTextFile('SparkPageRank.txt')
-    pageRankOrdered = ranks.sortBy(lambda a: -a[1])
-    pageRankOrdered.saveAsTextFile(sys.argv[2])
+    # Order the pages by PageRank and save the total rank in a file
+    pageRanksOrdered = ranks.takeOrdered(ranks.count(), key=lambda x: -x[1])
+    pageRanksOrdered = sc.parallelize(pageRanksOrdered)
+    pageRankOrdered.saveAsTextFile(sys.argv[3])
     sc.stop()
